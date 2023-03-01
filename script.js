4a043e4bca0cc25c087e97b3c5d82f50c61d913c
nurlanahasanzada1711 <126058860+nurlanahasanzada1711@users.noreply.github.com>
2023-03-01 22:08:21 +0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+       let a = [25, 56, 112, 33, 67];
+       let cem = 0;
+       for(let i = 0; i < a.length; i++)
+       {
+           if(a[i] % 2 != 0)
+           {
+               cem += a[i];
+           }
+        }
+            console.log(cem); 
