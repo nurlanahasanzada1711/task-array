8eed376357c2213533b8ca5d1eaaf504879dd9fd
nurlanahasanzada17 <6ea7n3z@code.edu.az>
2023-03-01 22:06:16 +0400

    hello

---
+++ task array 2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace task_array_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 55, 79, 38, 22, 106 };
+            int cem = 0;
+            int say = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                say++;
+                cem += a[i];
+            }
+            Console.WriteLine(cem / say);
+        }
+    }
+}
---
+++ task array/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace task_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 25, 56, 112, 33, 67 };
+            int cem = 0;
+            for (int i = 0; i < a.Length; i++)
+            {
+                if (a[i] % 2 != 0)
+                {
+                    cem += a[i];
+                }
+            }
+            Console.WriteLine(cem);
+        }
+    }
+}
